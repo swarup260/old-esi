cc1af96e65d5e041abb6a194665b254f5310034f
swarup das <swarup260@gmail.com>
2020-05-04 22:08:35 +0530

    url overflow and url launcher

--- lib/ServicesDetails.dart
+++ lib/ServicesDetails.dart
@@ -1,12 +1,12 @@
 import 'package:admob_flutter/admob_flutter.dart';
-import 'package:esi_essential_services_india/api/apiEndpoint.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import 'api/apiEndpoint.dart';
 import 'models/resources.dart';
 import 'widgets/AppDrawer.dart';
-import './api/networkManager.dart';
+import 'api/networkManager.dart';
 
 class ServicesDetails extends StatelessWidget {
   ServicesDetails(
@@ -23,7 +23,7 @@ class ServicesDetails extends StatelessWidget {
         title: Text(this.category),
       ),
       body: FutureBuilder(
-        future: this.filteredCategoryList(this.servicesList),
+        future: this._filteredCategoryList(this.servicesList),
         initialData: servicesList,
         builder:
             (BuildContext context, AsyncSnapshot<List<Resource>> snapshot) {
@@ -53,7 +53,7 @@ class ServicesDetails extends StatelessWidget {
     );
   }
 
-  Future<List<Resource>> filteredCategoryList(List<Resource> list) async {
+  Future<List<Resource>> _filteredCategoryList(List<Resource> list) async {
     /* Return Only the select State & City Data */
     Map<String, dynamic> stateCityMap = await getPersistedStateCity();
     // Map <String,String> stateCityMap = {"state" : "Maharashtra" , "city" : "Mumbai"};
@@ -85,8 +85,7 @@ class ServiceListTile extends StatelessWidget {
     return ListView.builder(
         itemCount: this.filteredList.length,
         itemBuilder: (BuildContext context, int index) {
-          // List contact = this.filteredList[index].phonenumber.split(',');
-          List contact = "8689886323,8689886323,8689886323,8689886323".split(',');
+          List contact = this.filteredList[index].phonenumber.split(',');
           return Card(
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(3.0),
@@ -100,43 +99,91 @@ class ServiceListTile extends StatelessWidget {
                         style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 15.0)),
                   ),
-                  subtitle: Row(children: <Widget>[ 
-                    for (var i = 0; i < 2; i++)
-                    InkWell(
-                            onTap: () {
-                              launch("tel://" + contact[i]);
-                            },
-                            child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: <Widget>[
-                                  Icon(Icons.call),
-                                  Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 5.0, bottom: 10.0),
-                                      child: Text(contact[i]))
-                                ]))
-                  ],),
+                  subtitle: ContactWidget(contact: contact),
                   children: <Widget>[
-                    ListTile(
-                      title: Padding(
-                          padding: const EdgeInsets.only(left: 10.0, top: 10.0),
-                          child: Text(this
-                              .filteredList[index]
-                              .descriptionandorserviceprovided)),
-                      subtitle: Padding(
-                        padding: const EdgeInsets.only(
-                            left: 10.0, bottom: 5.0, top: 10.0),
-                        child: Row(children: <Widget>[
-                          Icon(Icons.insert_link),
-                          SizedBox(
-                            width: 5.0,
-                          ),
-                          Text(this.filteredList[index].contact)
-                        ]),
-                      ),
-                    )
+                    ServiceDescription(filteredList: filteredList, index: index)
                   ]));
         });
+  }
+}
+
+class ContactWidget extends StatelessWidget {
+  const ContactWidget({
+    Key key,
+    @required this.contact,
+  }) : super(key: key);
+
+  final List<String> contact;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        for (var i = 0; i < this.contact.length && i < 2; i++)
+          InkWell(
+              onTap: () {
+                launch("tel://" + contact[i]);
+              },
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Icon(Icons.call),
+                    Padding(
+                        padding: const EdgeInsets.only(left: 5.0, bottom: 10.0 , right: 10.0),
+                        child: Text(contact[i].trim() ))
+                  ])),
+      ],
+    );
+  }
+}
+
+class ServiceDescription extends StatelessWidget {
+  const ServiceDescription({
+    Key key,
+    @required this.filteredList,
+    @required this.index,
+  }) : super(key: key);
+
+  final List<Resource> filteredList;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Padding(
+          padding: const EdgeInsets.only(left: 10.0, top: 10.0),
+          child:
+              Text(this.filteredList[index].descriptionandorserviceprovided)),
+      subtitle: this.filteredList[index].contact.length > 0
+          ? Padding(
+              padding:
+                  const EdgeInsets.only(left: 10.0, bottom: 5.0, top: 10.0),
+              child: Row(children: <Widget>[
+                InkWell(
+                  onTap: () {
+                    _launchURL(this.filteredList[index].contact);
+                  },
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.insert_link),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                      SizedBox(width: 300 ,child: Text(this.filteredList[index].contact,overflow: TextOverflow.ellipsis),)
+                    ],
+                  ),
+                )
+              ]),
+            )
+          : Text(''),
+    );
+  }
+
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
